848aa4c834ad107c399367da32fc6cebf452e906
biotoxick <alex@DESKTOP-I98T0KU.home>
2022-06-16 14:41:16 +0200

    ajout chassis vide

--- src/main/java/jpomc/init/JpomcModItems.java
+++ src/main/java/jpomc/init/JpomcModItems.java
@@ -18,7 +18,6 @@ import jpomc.item.ProcessorItem;
 import jpomc.item.MouseItem;
 import jpomc.item.MotherboardItem;
 import jpomc.item.GraphicCardItem;
-import jpomc.item.ChassisItem;
 import jpomc.item.AlimentationItem;
 import jpomc.item.AircoolingItem;
 
@@ -26,7 +25,6 @@ import jpomc.JpomcMod;
 
 public class JpomcModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, JpomcMod.MODID);
-	public static final RegistryObject<Item> CHASSIS = REGISTRY.register("chassis", () -> new ChassisItem());
 	public static final RegistryObject<Item> MOTHERBOARD = REGISTRY.register("motherboard", () -> new MotherboardItem());
 	public static final RegistryObject<Item> GRAPHIC_CARD = REGISTRY.register("graphic_card", () -> new GraphicCardItem());
 	public static final RegistryObject<Item> RAM = REGISTRY.register("ram", () -> new RAMItem());
@@ -35,6 +33,7 @@ public class JpomcModItems {
 	public static final RegistryObject<Item> AIRCOOLING = REGISTRY.register("aircooling", () -> new AircoolingItem());
 	public static final RegistryObject<Item> MOUSE = REGISTRY.register("mouse", () -> new MouseItem());
 	public static final RegistryObject<Item> CLAVIER = block(JpomcModBlocks.CLAVIER, JpomcModTabs.TAB_JPOMC);
+	public static final RegistryObject<Item> CHASSIS_VIDE = block(JpomcModBlocks.CHASSIS_VIDE, JpomcModTabs.TAB_JPOMC);
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
